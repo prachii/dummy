38f9fdaa495615f8c03a66ee57adfd2f1120046c
Prachi Vohra <prachi9293@hotmail.com>
2014-08-22 16:52:32 +0530

    Enter Name

--- name.py
+++ name.py
@@ -1,5 +1,5 @@
 while True:
-	Firstname=raw_input('Enter your firstname : ')
+	Firstname=raw_input('Enter your name : ')
 	Lastname=raw_input('Enter your lastname : ')
 	dict={'First name':Firstname, ' Last name':Lastname}
 	if Firstname == "" and Lastname == "":
